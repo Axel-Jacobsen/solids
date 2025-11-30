615ead391442f3208f170e93cd8789c42348bfdf
Axel Jacobsen <axelnj44@gmail.com>
2025-11-30 15:02:21 -0800

    oscillating spring coverges worse

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,9 @@ type VertexId = usize;
 type Neighbors = std::collections::HashMap<VertexId, Vec<VertexId>>;
 
 type Locations = std::collections::HashMap<VertexId, Point3<f64>>;
-type Velocities = std::collections::HashMap<VertexId, Vector3<f64>>;
 type Forces = std::collections::HashMap<VertexId, Vector3<f64>>;
 
-fn random_point_in_unit_cube() -> Point3<f64> {
+fn random_point() -> Point3<f64> {
     let mut rng = rand::rng();
     let (x, y, z): (f64, f64, f64) = rng.random();
     Point3::new(x, y, z)
@@ -43,9 +42,7 @@ fn neighbors_for_solid(solid: &PlatonicSolid) -> Neighbors {
 struct RelaxParams {
     spring_constant: f64,
     natural_length: f64,
-    damping_constant: f64,
-    vertex_mass: f64,
-    dt: f64,
+    step_size: f64,
     total_movement_thresh: f64,
 }
 
@@ -54,18 +51,12 @@ fn relax(neighbors: &Neighbors, relax_params: RelaxParams) -> Locations {
     let RelaxParams {
         spring_constant,
         natural_length,
-        damping_constant,
-        vertex_mass,
-        dt,
+        step_size,
         total_movement_thresh,
     } = relax_params;
 
-    let mut locations: Locations = std::collections::HashMap::from_iter(
-        neighbors.keys().map(|k| (*k, random_point_in_unit_cube())),
-    );
-    let mut velocities: Velocities = std::collections::HashMap::from_iter(
-        neighbors.keys().map(|k| (*k, Vector3::new(0.0, 0.0, 0.0))),
-    );
+    let mut locations: Locations =
+        std::collections::HashMap::from_iter(neighbors.keys().map(|k| (*k, random_point())));
     let mut forces: Forces = std::collections::HashMap::from_iter(
         neighbors.keys().map(|k| (*k, Vector3::new(0.0, 0.0, 0.0))),
     );
@@ -90,10 +81,6 @@ fn relax(neighbors: &Neighbors, relax_params: RelaxParams) -> Locations {
                 *forces.get_mut(vertex).unwrap() +=
                     force_mag * (neighbor_location - this_vertex_location).normalize()
             }
-
-            // Damping.
-            let this_vertex_velocity = velocities.get(vertex).unwrap();
-            *forces.get_mut(vertex).unwrap() -= damping_constant * this_vertex_velocity;
         }
 
         // Update states.
@@ -102,19 +89,23 @@ fn relax(neighbors: &Neighbors, relax_params: RelaxParams) -> Locations {
         // a = dv/dt = (v1 - v0) / t so v1 = at + v0
         let mut total_movement = 0.0;
         for vertex in neighbors.keys() {
-            // Calculate acceleration.
-            let acceleration = forces.get(vertex).unwrap() / vertex_mass;
-
-            // Calculate displacement from current position.
-            let movement = velocities.get(vertex).unwrap() * dt + 0.5 * acceleration * dt * dt;
-
             // Update position and velocity.
+            let movement = forces[vertex] * step_size;
             *locations.get_mut(vertex).unwrap() += movement;
-            *velocities.get_mut(vertex).unwrap() += acceleration * dt;
-            total_movement += movement.magnitude();
+            total_movement += movement.norm();
         }
 
-        if total_movement < total_movement_thresh {
+        // Recenter.
+        let mut centroid = Vector3::new(0.0, 0.0, 0.0);
+        for p in locations.values() {
+            centroid += p.coords;
+        }
+        centroid /= locations.len() as f64;
+        for p in locations.values_mut() {
+            p.coords -= centroid;
+        }
+
+        if total_movement / (neighbors.len() as f64) < total_movement_thresh {
             break;
         }
     }
@@ -128,11 +119,9 @@ fn main() {
         let locs = relax(
             neighbors,
             RelaxParams {
-                spring_constant: 10.0,
+                spring_constant: 100.0,
                 natural_length: 1.0,
-                damping_constant: 1.0,
-                vertex_mass: 1.0,
-                dt: 1e-3,
+                step_size: 1e-3,
                 total_movement_thresh: 1e-6,
             },
         );
